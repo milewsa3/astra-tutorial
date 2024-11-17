958fabcf53ed04fca4c13ae04e0ce97fe4d35c93
Adrian <a.milewski@onet.eu>
2024-11-17 19:01:52 +0100

    Initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+    ],
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
